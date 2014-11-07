4559a6cd7f8eb83e65b511a21d7902a9f8b27185
RahulKhanna <sociallyrk@gmail.com>
2014-11-06 16:43:26 -0800

    added stores to rails

--- app/models/loan.rb
+++ app/models/loan.rb
@@ -6,13 +6,6 @@ class Loan < ActiveRecord::Base
   def generate_loanhash
     response = Typhoeus.get("https://api.consumerfinance.gov/data/hmda/slice/hmda_lar.json?#!/lien_status=1&loan_purpose=1&action_taken=1&select=as_of_year,loan_type_name,count&section=summary")
     result = JSON.parse(response.body)
-    puts "@Loan:"
-    puts self.link
-    puts "inspect:"
-    #puts result.inspect
-    self.properties = "hello world"
-    return "done"
-    # @loan.loanhash = result
-    # puts @loan.loanhash
+    self.properties = result
   end
 end
--- db/migrate/20141106224235_add_columns2_to_loans.rb
+++
@@ -1,5 +0,0 @@
-class AddColumns2ToLoans < ActiveRecord::Migration
-  def change
-    add_column :loans, :hash_data, :hstore
-  end
-end
